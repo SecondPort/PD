82231b336d5ff4089a1fd93d2138c83da90bd848
SecondPort <gomezmanulm@gmail.com>
2022-08-17 18:13:26 -0300

    un poco de codigo

---
+++ Introduccion/inicio.hs
@@ -0,0 +1,44 @@
+cuadrado::Integer -> Integer
+cuadrado x = x * x
+
+--funcion boolean
+esPar :: Integer -> Bool
+esPar x = mod x 2 == 0
+
+esParTexto :: Integer -> String
+esParTexto x
+    | esPar x = "par"
+    | otherwise = "impar"
+
+menor::(Integer, Integer) -> Integer
+menor(x,y) = if x < y then x else y
+
+--Redefinir la función abs (sin usar la predefinida) tal que abs x es el valor absoluto de x del lenguaje haskell
+abs :: Integer -> Integer
+abs x
+    | x < 0 = -x
+    | otherwise = x
+
+--Redefinir la función signum tal que signum x es -1 si x es negativo, 0 si x es cero y 1 si x es positivo.
+signum :: Integer -> Integer
+signum x = if x < 0 then -1 else if x == 0 then 0 else 1
+
+--Definir la función anterior tal que anterior x es el anterior del número natural x.
+anterior :: Integer -> Integer
+anterior x = x - 1
+
+
+--Definir la función siguiente tal que siguiente x sea el siguiente del número entero x
+siguiente :: Integer -> Integer
+siguiente x = x + 1
+
+--Definir la función doble tal que doble x es el doble de x
+doble :: Integer -> Integer
+doble x = x * 2
+
+--filterPositivos: dada una lista de enteros, devuelve una lista con los elementos que son positivos. (usar recursividad)
+filterPositivos :: [Integer] -> [Integer]
+filterPositivos [] = []
+filterPositivos (x:xs)
+    | x > 0 = x : filterPositivos xs
+    | otherwise = filterPositivos xs

bf3422e451e6bcfa9cc7e40b247f870ec9520c80
SecondPort <gomezmanulm@gmail.com>
2022-09-09 19:45:46 -0300

    clase 09/09/2022

--- Prolog/Inicio/init.pl
+++ Prolog/Inicio/init.pl
@@ -1,2 +1,12 @@
-tiene(puerta,ventana).
+esPato(lucas).
+
+primer_elemento([X|_],X).
+
+%hallar el ultimo elemento de la lista%
+ultimo_elemento([X],X).
+ultimo_elemento([_|Y],X):-ultimo_elemento(Y,X).
+
+%hallar el elemento k de una lista%
+elemento_k([X|_],0,X).
+elemento_k([_|Y],K,X):-K1 is K-1,elemento_k(Y,K1,X).
 
